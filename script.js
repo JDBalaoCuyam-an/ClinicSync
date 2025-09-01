7ce029b075e647e6663de309855751f0847b8a0e
cuyaman <2023-01-0030@kcp.edu.ph>
2025-09-01 23:18:23 +0800

    MedicineForm_CloseOpen

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 function toggleSidebar() {
   document.getElementById("sidebar").classList.toggle("show");
 }
+
+// Medicine Inventory Functions
+function addMedicine() {
+  document.getElementById("add-medicine").classList.add("show");
+}
+
+// Close Button Function
+function closeButton() {
+  if (document.getElementById("add-medicine").classList.contains("show")) {
+    document.getElementById("add-medicine").classList.remove("show");
+  }
+}
