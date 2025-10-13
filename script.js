04108112cf91cf911d8ce19449e88b701dfc5429
cuyaman <2023-01-0030@kcp.edu.ph>
2025-10-13 09:56:44 +0800

    added new Consultations record Modal

--- script.js
+++ script.js
@@ -22,12 +22,17 @@ function addPatientOverlay() {
   document.getElementById("add-new-patient").classList.add("show");
   document.getElementById("overlay").classList.add("show");
 }
+function addConsultationRecordOverlay() {
+  document.getElementById("add-consultation-record").classList.add("show");
+  document.getElementById("overlay").classList.add("show");
+}
 // Close Button Function and Background Click Blocker
 function closeButtonOverlay() {
   const addMedicine = document.getElementById("add-medicine");
   const dispenseMedicine = document.getElementById("dispense-medicine");
   const addBorrower = document.getElementById("add-borrower");
   const addPatient = document.getElementById("add-new-patient");
+  const addConsultationRecord = document.getElementById("add-consultation-record");
   const overlay = document.getElementById("overlay");
 
   if (addMedicine && addMedicine.classList.contains("show")) {
@@ -41,6 +46,9 @@ function closeButtonOverlay() {
   }
   if (addPatient && addPatient.classList.contains("show")) {
     addPatient.classList.remove("show");
+  }
+  if (addConsultationRecord && addConsultationRecord.classList.contains("show")) {
+    addConsultationRecord.classList.remove("show");
   }
   if (overlay) {
     overlay.classList.remove("show");
